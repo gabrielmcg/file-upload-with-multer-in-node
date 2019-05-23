7e36628a8acb5b2a4738316e40a0c1937398fb39
root <root@hpe2-worker01.am2.cloudra.local>
2019-05-23 11:04:24 +0000

    prom metrics

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser= require('body-parser')
+const Prometheus = require('prom-client')
 const app = express()
 const multer = require('multer');
 fs = require('fs-extra')
@@ -9,6 +10,21 @@ const MongoClient = require('mongodb').MongoClient
 ObjectId = require('mongodb').ObjectId
 
 const myurl = 'mongodb://mongodb:27017';
+
+const metricsInterval = Prometheus.collectDefaultMetrics()
+
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]  // buckets for response time from 0.1ms to 500ms
+})
+
+// Runs before each requests
+app.use((req, res, next) => {
+  res.locals.startEpoch = Date.now()
+  next()
+})
 
 
 var storage = multer.diskStorage({
@@ -35,7 +51,7 @@ app.listen(3000, () => {
   console.log('listening on 3000')
 })
 
-app.get('/mongoconnect',function(req,res){
+app.get('/mongoconnect',function(req,res, next){
 MongoClient.connect(myurl, (err, client) => {
   if (err) return console.log(err)
   db = client.db('test')
@@ -44,15 +60,17 @@ MongoClient.connect(myurl, (err, client) => {
   res.redirect('/uploadui')
 
 
+   next();
 })
 
 
 });
 
 
-app.get('/uploadui',function(req,res){
+app.get('/uploadui',function(req,res, next){
   res.sendFile(__dirname + '/index.html');
 
+   //next();
 });
 
 // upload single file
@@ -69,6 +87,7 @@ app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
  
     res.send(file)
  
+   next();
 })
 //Uploading multiple files
 app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
@@ -81,9 +100,10 @@ app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
 
     res.send(files)
  
+   next();
 })
 
-app.post('/uploadphoto', upload.single('picture'), (req, res) => {
+app.post('/uploadphoto', upload.single('picture'), (req, res, next) => {
 	var img = fs.readFileSync(req.file.path);
  var encode_image = img.toString('base64');
  // Define a JSONobject for the image attributes for saving to database
@@ -101,11 +121,12 @@ app.post('/uploadphoto', upload.single('picture'), (req, res) => {
     res.redirect('/uploadui')
   
     
+   next();
   })
 })
 
 
-app.get('/photos', (req, res) => {
+app.get('/photos', (req, res, next) => {
 db.collection('mycollection').find().toArray((err, result) => {
 
   	const imgArray= result.map(element => element._id);
@@ -115,11 +136,12 @@ db.collection('mycollection').find().toArray((err, result) => {
    res.send(imgArray)
 
   
+   next();
    
   })
 });
 
-app.get('/photo/:id', (req, res) => {
+app.get('/photo/:id', (req, res, next) => {
 var filename = req.params.id;
 
 db.collection('mycollection').findOne({'_id': ObjectId(filename) }, (err, result) => {
@@ -130,6 +152,27 @@ db.collection('mycollection').findOne({'_id': ObjectId(filename) }, (err, result
    res.send(result.image.buffer)
   
    
+   next();
   })
 })
+
+
+app.get('/metrics', (req, res, next) => {
+  res.set('Content-Type', Prometheus.register.contentType)
+  res.end(Prometheus.register.metrics())
+})
+
+
+// Runs after each requests
+app.use((req, res, next) => {
+  const responseTimeInMs = Date.now() - res.locals.startEpoch
+
+  httpRequestDurationMicroseconds
+    .labels(req.method, req.route.path, res.statusCode)
+    .observe(responseTimeInMs)
+
+  next()
+})
+
+
 
